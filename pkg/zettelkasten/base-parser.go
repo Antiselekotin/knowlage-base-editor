452be6f69c87ab6e1b95416d16b65d793aa32cb0
Antiselekotin <deksazon8@gmail.com>
2022-01-31 17:23:15 +0200

    simplify base parser

--- pkg/zettelkasten/base-parser.go
+++ pkg/zettelkasten/base-parser.go
@@ -6,7 +6,12 @@ import (
 	"strings"
 )
 
-var articlesDir, tagsDir = "base", "tags"
+var (
+	articlesDir, tagsDir = "base", "tags"
+	articlePathSubstring = "(../base/"
+	tagsPathSubstring = "(../../tags/"
+	connPathSubstring = "(../"
+)
 
 func (b *base) ParseFromDisk(pathToRoot string) error {
 	absPath, err := filepath.Abs(pathToRoot)
@@ -26,11 +31,11 @@ func (b *base) ParseFromDisk(pathToRoot string) error {
 	if !dirMap[articlesDir] || !dirMap[tagsDir] {
 		return ErrNoReqDirs
 	}
-	tagsClear, err := parseTagFiles(absPath)
+	tagsClear, err := parseAllTags(absPath)
 	if err != nil {
 		return err
 	}
-	articlesClear, err := parseArticleFiles(absPath)
+	articlesClear, err := parseAllArticles(absPath)
 	if err != nil {
 		return err
 	}
@@ -43,110 +48,126 @@ func (b *base) ParseFromDisk(pathToRoot string) error {
 	return nil
 }
 
-func parseTagFiles(absPath string) ([]tagClear, error) {
+func parseAllTags(absPath string) ([]tagClear, error) {
 	tagPath := filepath.Join(absPath, tagsDir)
 	tagsDirContent, err := os.ReadDir(tagPath)
 	if err != nil {
 		return nil, err
 	}
-	tagsContent := make([]tagClear, 0)
-	articlePathSubstring := "(../base/"
+
+	tagsContent := make([]tagClear, 0, len(tagsDirContent))
 	for _, file := range tagsDirContent {
-		if !file.IsDir() {
-			data, err := os.ReadFile(filepath.Join(filepath.Join(tagPath, file.Name())))
-			if err != nil {
-				return nil, err
-			}
-			strData := string(data)
-			fileLines := strings.Split(strData, "\n")
-			if len(fileLines) == 0 {
-				return nil, ErrTagFileNotCorrect
-			}
-			if strings.HasPrefix(fileLines[0], "# ") {
-				tagContent := tagClear{
-					Title:        fileLines[0][2:],
-					FileName:     file.Name(),
-					ArticlePaths: make([]string, 0),
-				}
-				for _, line := range fileLines {
-					index := strings.Index(line, articlePathSubstring)
-					if index != -1 {
-						tagContent.ArticlePaths = append(tagContent.ArticlePaths,
-							strings.ReplaceAll(line[index+len(articlePathSubstring):len(line)-1], "/", ""))
-					}
-				}
-				tagsContent = append(tagsContent, tagContent)
-			} else {
-				return nil, ErrTagFileNotCorrect
-			}
+		tag, err := parseTagFile(file, tagPath)
+		if err != nil {
+			return nil, err
 		}
+		tagsContent = append(tagsContent, tag)
 	}
 	return tagsContent, nil
 }
 
-func parseArticleFiles(absPath string) ([]articleClear, error) {
+func parseAllArticles(absPath string) ([]articleClear, error) {
 	articlePath := filepath.Join(absPath, articlesDir)
 	articleDirContent, err := os.ReadDir(articlePath)
-	tagsPathSubstring := "(../../tags/"
-	connPathSubstring := "(../"
-	articlesContent := make([]articleClear, 0)
 	if err != nil {
 		return nil, err
 	}
+
+	articlesContent := make([]articleClear, 0, len(articleDirContent))
 	for _, dir := range articleDirContent {
-		if dir.IsDir() {
-			articleBt, err := os.ReadFile(filepath.Join(articlePath, dir.Name(), "README.md"))
-			if err != nil {
-				return nil, err
-			}
-			articleStr := string(articleBt)
-			lines := strings.Split(articleStr, "\n")
-			if len(lines) == 0 {
-				return nil, ErrArticleFileNotCorrect
-			}
-			if strings.HasPrefix(lines[0], "# ") {
-				articleContent := articleClear{
-					Title:        lines[0][2:],
-					FileName:     dir.Name(),
-					ArticlePaths: make([]string, 0),
-					TagPaths:     make([]string, 0),
-				}
-				iterStat := map[string]bool{}
-				for lineIndex, line := range lines {
-					if strings.HasPrefix(line, "## ") {
-						if strings.HasPrefix(line[3:], "Теги") {
-							iterStat["isTagSection"] = true
-							iterStat["isConnectionSection"] = false
-							continue
-						} else if strings.HasPrefix(line[3:], "Связи") {
-							iterStat["isTagSection"] = false
-							iterStat["isConnectionSection"] = true
-							continue
-						} else {
-							articleContent.Content = strings.Join(lines[lineIndex:], "\n")
-							break
-						}
-					}
-					if iterStat["isTagSection"] {
-						index := strings.Index(line, tagsPathSubstring)
-						if index != -1 {
-							articleContent.TagPaths = append(articleContent.TagPaths,
-								line[index+len(tagsPathSubstring):len(line)-1])
-						}
-					}
-					if iterStat["isConnectionSection"] {
-						index := strings.Index(line, connPathSubstring)
-						if index != -1 {
-							articleContent.ArticlePaths = append(articleContent.ArticlePaths,
-								line[index+len(connPathSubstring):len(line)-1])
-						}
-					}
-				}
-				articlesContent = append(articlesContent, articleContent)
-			} else {
-				return nil, ErrArticleFileNotCorrect
-			}
+		article, err := parseArticleFile(dir, articlePath)
+		if err != nil {
+			return nil, err
 		}
+		articlesContent = append(articlesContent, article)
 	}
 	return articlesContent, nil
+}
+
+func parseTagFile(file os.DirEntry, tagPath string) (tagClear, error) {
+	tagContent := tagClear{
+		Title:        "",
+		FileName:     file.Name(),
+		ArticlePaths: make([]string, 0),
+	}
+	if !file.IsDir() {
+		data, err := os.ReadFile(filepath.Join(filepath.Join(tagPath, file.Name())))
+		if err != nil {
+			return tagContent, err
+		}
+		lines := strings.Split(string(data), "\n")
+		if len(lines) == 0 {
+			return tagContent, ErrTagFileNotCorrect
+		}
+		if strings.HasPrefix(lines[0], "# ") {
+			tagContent.Title = lines[0][2:]
+			for _, line := range lines[1:] {
+				if cut, ok := cutPathFromLine(line, articlePathSubstring); ok {
+					tagContent.ArticlePaths = append(tagContent.ArticlePaths, cut)
+				}
+			}
+			return tagContent, nil
+		}
+	}
+	return tagContent, ErrTagFileNotCorrect
+}
+
+func parseArticleFile(dir os.DirEntry, articlePath string) (articleClear, error) {
+	articleContent := articleClear{
+		Title:        "",
+		FileName:     dir.Name(),
+		ArticlePaths: make([]string, 0),
+		TagPaths:     make([]string, 0),
+	}
+
+	if dir.IsDir() {
+		article, err := os.ReadFile(filepath.Join(articlePath, dir.Name(), "README.md"))
+		if err != nil {
+			return articleContent, err
+		}
+		lines := strings.Split(string(article), "\n")
+
+		if len(lines) == 0 {
+			return articleContent, ErrArticleFileNotCorrect
+		}
+
+		if strings.HasPrefix(lines[0], "# ") {
+			articleContent.Title = lines[0][2:]
+			for lineIndex, line := range lines[1:] {
+				iterStat := map[string]bool{}
+				if strings.HasPrefix(line, "## ") {
+					if strings.HasPrefix(line[3:], "Теги") {
+						iterStat["isTagSection"], iterStat["isConnectionSection"] = true, false
+						continue
+					} else if strings.HasPrefix(line[3:], "Связи") {
+						iterStat["isTagSection"], iterStat["isConnectionSection"] = false, true
+						continue
+					} else {
+						articleContent.Content = strings.Join(lines[lineIndex:], "\n")
+						break
+					}
+				}
+				if iterStat["isTagSection"] {
+					if cut, ok := cutPathFromLine(line, tagsPathSubstring); ok {
+						articleContent.TagPaths = append(articleContent.TagPaths, cut)
+					}
+				}
+				if iterStat["isConnectionSection"] {
+					if cut, ok := cutPathFromLine(line, connPathSubstring); ok {
+						articleContent.ArticlePaths = append(articleContent.ArticlePaths, cut)
+					}
+				}
+			}
+			return articleContent, nil
+		}
+	}
+	return articleContent, ErrArticleFileNotCorrect
+}
+
+func cutPathFromLine(line, subString string) (cut string, ok bool) {
+	index := strings.Index(line, subString)
+	if index != -1 {
+		return strings.ReplaceAll(line[index+len(subString):len(line)-1], "/", ""), ok
+	}
+	return
 }
