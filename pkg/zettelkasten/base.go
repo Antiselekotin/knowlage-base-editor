a6d11c2b540b42f9684dfec1cede260aec367561
Antiselekotin <deksazon8@gmail.com>
2022-01-31 17:35:19 +0200

    correct add tag and article

--- pkg/zettelkasten/base.go
+++ pkg/zettelkasten/base.go
@@ -6,19 +6,27 @@ import (
 )
 
 type base struct {
-	Articles map[string]*Article
-	Tags map[string]*Tag
+	articles map[string]*Article
+	tags map[string]*Tag
+}
+
+func NewBase() *base {
+	return &base{
+		tags: make(map[string]*Tag),
+		articles: make(map[string]*Article),
+	}
 }
 
 func (b *base)AddTag(tag tagClear) {
 	newTag := Tag{Title: tag.Title, FileName: tag.FileName, Articles: make([]*Article, 0)}
 	for _, articleTitle := range tag.ArticlePaths {
-		article, ok := b.Articles[articleTitle]
+		article, ok := b.articles[articleTitle]
 		if ok {
 			newTag.Articles = append(newTag.Articles, article)
 			article.Tags = append(article.Tags, &newTag)
 		}
 	}
+	b.tags[newTag.Title] = &newTag
 }
 
 func (b *base)AddArticle(article articleClear) {
@@ -34,24 +42,38 @@ func (b *base)AddArticle(article articleClear) {
 	newArticle := Article{Title: article.Title, FileName: fName, Content: article.Content, Number: num,
 		Tags: make([]*Tag, 0), Connections: make([]*Article, 0)}
 		for _, articleTitle := range article.ArticlePaths {
-			article, ok := b.Articles[articleTitle]
+			article, ok := b.articles[articleTitle]
 			if ok {
 				newArticle.Connections = append(newArticle.Connections, article)
 				article.Connections = append(article.Connections, &newArticle)
 			}
 		}
 	for _, tagTitle := range article.TagPaths {
-		tag, ok := b.Tags[tagTitle]
+		tag, ok := b.tags[tagTitle]
 		if ok {
 			newArticle.Tags = append(newArticle.Tags, tag)
 			tag.Articles = append(tag.Articles , &newArticle)
 		}
 	}
+	b.articles[newArticle.Title] = &newArticle
 }
 
-func NewBase() *base {
-	return &base{
-		Tags: make(map[string]*Tag),
-		Articles: make(map[string]*Article),
+func (b *base)Tags() []*Tag {
+	tags := make([]*Tag, len(b.tags))
+	i := 0
+	for _, tag := range b.tags {
+		tags[i] = tag
+		i++
 	}
+	return tags
+}
+
+func (b *base)Articles() []*Article {
+	articles := make([]*Article, len(b.articles))
+	i := 0
+	for _, article := range b.articles {
+		articles[i] = article
+		i++
+	}
+	return articles
 }
